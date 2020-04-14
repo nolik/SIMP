80a708df9d6c15388861708a229fa1e2801c9063
Ihar Novik <i.novik@godeltech.com>
2020-04-14 12:07:55 +0300

    Refactoring.

--- lab8/src/com/novik/Main.java
+++ lab8/src/com/novik/Main.java
@@ -12,11 +12,20 @@ public class Main {
 
   public static void main(String[] args) {
     final var newText = List.of(
-        List.of("A", "huck", "up!"),
+        List.of("A", "wake", "up!"),
+        List.of("Just", "hakooona", "matata!!!"),
         List.of("It's", "a", "wonderful", "huck."),
         List.of("It's", "a", "spring", "time", "saddens."));
 
-    modifyText(newText, 'h', 'k');
+    System.out.println("Исходный текст: \n" + newText.stream()
+        .flatMap(Collection::stream)
+        .collect(Collectors.joining(" ")));
+
+    final var firstLiteral = 'h';
+    final var secondLiteral = 'c';
+    System.out.printf("Литералы для удаления текста - первый: %s, второй: %s%n", firstLiteral, secondLiteral);
+
+    modifyText(newText, firstLiteral, secondLiteral);
   }
 
   public static void modifyText(List<List<String>> text, char firstLiteral, char secondLiteral) {
@@ -26,7 +35,6 @@ public class Main {
             WORD_REGEXP_FUNCTION.apply(firstLiteral, secondLiteral), ""))
         .collect(Collectors.joining(" "));
 
-    System.out.println(resultedText);
+    System.out.println("Модифицированный текст: \n" + resultedText);
   }
-
 }
